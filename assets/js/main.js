0f8e4bf96623f96e4ac79a02b8b2cc84f7e4de57
andrea-everett <andrea.dawn.everett@gmail.com>
2023-05-02 22:12:57 -0400

    format images

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,7 +129,37 @@
 })(jQuery);
 
 
-
+// GET https://graph.instagram.com/{media-id}
+//   ? fields = { fields }
+//   & access_token={ access - token }
 
 // // Instagram API
 // https://api.instagram.com/oauth/authorize?client_id=6074440476002258&redirect_uri=https://alanapartydecor.github.io/&scope=user_profile,user_media&response_type=code
+
+// var request = new XMLHttpRequest();
+// request.open('GET', 'https://graph.instagram.com/{media-id}?&access_token=IGQVJVdDFobVNvVEwzcVhlS3ZACZAmVyLWdXNlZASQVM0WG5WMG9xeXJCcWVHV1hxMlBHMFRHMzMyWTlDVmZAWdU5WYml3RE1yNF8zb2NMeGdvZAERTQWRfUTkxWkZALR2x5RkJlUlFScGd2MkR4WUJmcXZAPdwZDZD', true);
+
+// request.onload = function (container) {
+//   if (request.status >= 200 && request.status < 400) {
+//     // Success!
+//     var data = JSON.parse(request.responseText);
+//     for (var i = 0; i < data.data.length; i++) {
+//       var container = document.getElementById('insta-feed');
+//       var imgURL = data.data[i].images.standard_resolution.url;
+//       console.log(imgURL);
+//       var div = document.createElement('div');
+//       div.setAttribute('class', 'instapic');
+//       container.appendChild(div);
+//       var img = document.createElement('img');
+//       img.setAttribute('src', imgURL)
+//       div.appendChild(img);
+//     }
+
+//     console.log(data);
+//   } else {
+//   }
+// };
+// request.onerror = function () {
+//   // There was a connection error of some sort
+// };
+// request.send();
